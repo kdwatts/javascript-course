3bfc91848a4cbb3728a3920d24bf99780db53712
Kevin Watts <vtcpe2k4@gmail.com>
2017-12-29 21:50:07 -0500

    After Section 2
    No Comments

--- Section 2/script.js
+++ Section 2/script.js
@@ -101,7 +101,7 @@ switch (job){
   default:
     console.log("john does something else.");
 }
-*/
+
 
 var ageJohn = 35;
 var ageJohnFriend = 35;
@@ -126,3 +126,141 @@ else if (scoreJohn <= scoreJohnFriend && scoreJohnFriend > scoreExtraPerson)
 else if (scoreJohn < scoreExtraPerson && scoreJohnFriend < scoreExtraPerson)
     console.log("Extra Man Wins");
 else console.log ("Scoring Error");
+console.log("This is something new.");
+console.log("Where will it end.");
+
+
+
+function calculateAge(yearOfBirth){
+  var curYear = 2016;
+  var Age = curYear - yearOfBirth;
+  return Age;
+}
+
+var howOld = calculateAge(1990);
+console.log(howOld);
+
+ageMike = calculateAge(1999);
+ageMary = calculateAge(1969);
+console.log("Mike is age " + ageMike + " and Mary is " + ageMary);
+
+function yearsUntilRetirement(name, yearOfBirth){
+  console.log(name);
+  var age = calculateAge(yearOfBirth);
+  console.log(age);
+  if ((65-age)>= 0)
+    console.log(name + " has " + (65-age) + " years until retirement.");
+  else console.log(name + ' has already retired.');
+}
+
+yearsUntilRetirement('John', 1990);
+yearsUntilRetirement('Mike', 1969);
+yearsUntilRetirement('Mary', 1948);
+
+var name = ['John', 'Jane', 'Mark'];
+var birthYear = new Array(1990, 1969, 1948);
+var john = ['John', 'Smith', 1990, 'Designer', false];
+
+john.push('Blue');
+john.unshift("Mr.");
+john.pop();
+john.shift();
+console.log(john);
+console.log(john.indexOf('Smith'));
+if(john.indexOf('Teacher') === -1){
+  console.log("John is not a Teacher");
+}
+
+
+var john = {
+  name: "John",
+  lastName: "Smith",
+  yearOfBirth: 1990,
+   job: 'Teacher',
+   isMarried: false
+}
+console.log(john);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+john.lastName = 'Miller';
+john['job']= 'Programmer';
+console.log(john);
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1969
+jane.job = "Is Retired";
+jane.isMarried = true;
+console.log(jane);
+
+
+
+var john = {
+  name: "John",
+  lastName: "Smith",
+  yearOfBirth: 1990,
+  job: 'Teacher',
+  isMarried: false,
+  family:['Jane', 'Mark','Bob'],
+  calculateAge: function(){
+    return 2016 - this.yearOfBirth;
+  }
+}
+
+var mike = {
+  name: "Mike",
+  lastName: "Smith",
+  yearOfBirth: 1950,
+  job: 'Teacher',
+  isMarried: false,
+  family:['Jane', 'Mark','Bob'],
+  calculateAge: function(){
+    this.age = 2016 - this.yearOfBirth;
+  }
+};
+mike.calculateAge();
+console.log(mike);
+
+
+////Lecture Loops
+
+
+var names = ['John','Jane','Mary','Mark','Bob'];
+/*
+for(var i = (names.length - 1); i >= 0; i--){
+}
+
+var i = 0;
+while( i<names.length){
+  console.log(names[i]);
+  i++;
+}
+
+for(var i = 1; i<=5; i++){
+  console.log(i);
+  if (i === 3)
+    break;
+}
+
+*/
+
+var birthYears = [ 1965, 1972, 1991, 2005, 2016, 2015, 2000];
+var ages = [];
+//console.log(birthYears);
+//console.log(ages);
+for (var i = 0; i < birthYears.length; i++){
+  ages[i] = (2017 - birthYears[i]);
+  //console.log(ages);
+}
+ for(var i = 0; i<ages.length; i++){
+  var ofAge = false;
+  console.log(ages[i]);
+  console.log(ofAge);
+  if(ages[i] >= 18)
+    ofAge = true;
+  else ofAge = false;
+  console.log(ages[i]+ " "+ ofAge )
+}
